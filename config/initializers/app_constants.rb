efb797ce179e2c963c07f61d069f745ecbbc8c31
Geraldo Júnior <junior.bimbo@gmail.com>
2022-02-18 00:11:10 -0300

    chore: add home page

---
+++ config/initializers/app_constants.rb
@@ -0,0 +1,5 @@
+Rails.application.configure do
+  module AppConstants
+    CAIXA_LAST_RESULT_URL='http://www.loterias.caixa.gov.br/wps/portal/loterias/landing/federal/'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+  
+  get '/redirect_to_caixa_last_result_redirect', to: redirect(AppConstants::CAIXA_LAST_RESULT_URL)
 end
