33210bc89398b20af27614b06e122a86c0a04446
Geraldo Júnior <junior.bimbo@gmail.com>
2022-02-19 18:58:27 -0300

    chore: fetch sorted number

---
+++ app/javascript/custom/sorted_number.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('sorted_number');
+  const fetchNumberSortedUrl = form.dataset.sortedNumberUrl
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = [...formData.entries()];
+    const asQueryString = data.map(([key, value]) => `${key}=${value}`).join('&');
+
+    const url = `${fetchNumberSortedUrl}?${asQueryString}`;
+
+    fetchNumberSorted(url).then(result =>
+      printResult(result, '#result_quota_number')
+    );
+  });
+
+  const fetchNumberSorted = async (url) => {
+    const response = await fetch(url)
+      .then(response => response.json())
+      .then(json => json.sorted_number)
+
+    return response;
+  }
+
+  const printResult = (result, element) => {
+    const resultElement = document.querySelector(element);
+    resultElement.innerHTML = `Número da cota: ${result}`;
+    resultElement.classList.remove('d-none');
+  }
+});
--- app/models/federal_result.rb
+++ app/models/federal_result.rb
@@ -4,9 +4,13 @@ class FederalResult < ApplicationRecord
   validates :concurse, presence: true, uniqueness: true
   validates :date, :result, presence: true
 
+  def to_s
+    "[#{result.to_s.rjust(6, '0')}] - Concurso #{concurse} - #{date.strftime('%d/%m/%Y')}"
+  end
+
   def self.result_for_select
     all.order(concurse: :desc).limit(7).map do |federal_result|
-      ["[#{federal_result.result.to_s.rjust(6, '0')}] - Concurso #{federal_result.concurse} - #{federal_result.date.strftime('%d/%m/%Y')}", federal_result.result]
+      [federal_result.to_s, federal_result.result]
     end
   end
 end
