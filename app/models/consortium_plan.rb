36ce4c8ee6c02f3d30921e8cea6e592b52c5c52f
Geraldo Júnior <junior.bimbo@gmail.com>
2022-02-19 13:49:54 -0300

    fix: rubocop -A

--- app/models/consortium_plan.rb
+++ app/models/consortium_plan.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 class ConsortiumPlan < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   validates :installments, :person_count, presence: true
 
   def self.person_count_for_select
-    all.map { |plan| ["#{plan.name} - (Participantes: #{plan.person_count} em #{plan.installments} meses)", plan.person_count] }
+    all.map do |plan|
+      ["#{plan.name} - (Participantes: #{plan.person_count} em #{plan.installments} meses)", plan.person_count]
+    end
   end
 end
