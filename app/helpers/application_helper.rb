b4b3e009e4844b5a062fb685ec83744deec544b9
Geraldo Júnior <junior.bimbo@gmail.com>
2022-03-05 21:45:23 -0300

    chore: add page title tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def page_title_tag
+    content_tag('title') do
+      return content_for :page_title if content_for?(:page_title)
+
+      "#{I18n.t(:site_name)} -
+        #{I18n.t("#{controller_path.tr('/', '.')}.#{action_name}.title", default: nil)}"
+    end
+  end
 end
