2e15e56c724b58ea8ffd0feec919d926d66c128e
Geraldo Júnior <junior.bimbo@gmail.com>
2022-03-05 22:37:54 -0300

    chore: add developer_profile_link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,13 @@ module ApplicationHelper
         #{I18n.t("#{controller_path.tr('/', '.')}.#{action_name}.title", default: nil)}"
     end
   end
+
+  def developer_profile_link
+    link_to("by #{I18n.t('developer_profile.name')}",
+            I18n.t('developer_profile.profile_url'),
+            class: 'btn btn-outline-dark btn-sm',
+            title: I18n.t('developer_profile.btn_title'),
+            target: :_blank,
+            rel: :noopener)
+  end
 end
