2af966962dad116e2daa05d1e2045ed9d843f4d5
Geraldo Júnior <junior.bimbo@gmail.com>
2022-03-22 20:48:22 -0300

    chore: add sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_WEB_USERNAME"])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_WEB_PASSWORD"]))
+  end
+
+  mount Sidekiq::Web, at: "/sidekiq"
 
   # Defines the root path route ("/")
   root "home#index"
